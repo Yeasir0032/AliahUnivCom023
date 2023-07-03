5611abeb9790e7b13888b9483c971b4304aa763f
Yeasir0032 <124646897+Yeasir0032@users.noreply.github.com>
2023-07-03 05:36:35 +0000

    Sample.java added

---
+++ Sample.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Sample{
+  public static void main(String[] args){
+    System.out.println("Welcome -- ");
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    System.out.println("You have entered "+a);
+    
+  }
+}
