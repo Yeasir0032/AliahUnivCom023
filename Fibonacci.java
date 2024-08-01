a696f0a1d7fd9cd85bcf2f630cf2cfc8499ea6b7
MdISBAULHAQUE <mdisbaulh@gmail.com>
2024-08-02 00:13:57 +0530

    updated next.py and added a fibbonaci in java

---
+++ Fibonacci.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Fibonacci{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of terms: ");
+        int n = scanner.nextInt();
+        scanner.close();
+
+        int firstTerm = 0, secondTerm = 1;
+
+        System.out.print("Fibonacci Series: " + firstTerm + " " + secondTerm);
+
+        for (int i = 2; i < n; ++i) {
+            int nextTerm = firstTerm + secondTerm;
+            System.out.print(" " + nextTerm);
+            firstTerm = secondTerm;
+            secondTerm = nextTerm;
+        }
+    }
+}
